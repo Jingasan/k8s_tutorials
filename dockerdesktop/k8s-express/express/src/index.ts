2274b0337ffc36d2c4e313a75361da821fca77c3
Jingasan <vc@Jinga>
2023-07-07 02:33:04 +0900

    <add> initial commit

---
+++ dockerdesktop/k8s-express/express/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Application, Request, Response, NextFunction } from "express";
+import cors from "cors";
+const app: Application = express();
+const PORT = 3000;
+// リクエストボディのパース用設定
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// CORS設定
+app.use(cors());
+// GET
+app.get("/", async (req: Request, res: Response) => {
+  return res.status(200).send({
+    message: "Hello World!",
+  });
+});
+// サーバーを起動する処理
+try {
+  app.listen(PORT, () => {
+    console.log("server running at port:" + PORT);
+  });
+} catch (e) {
+  if (e instanceof Error) {
+    console.error(e.message);
+  }
+}
